ba528d1e126b0ae2520e9af8db650a4817729f7d
Pratham <prathameshagrawal12@gmail.com>
2025-11-06 23:27:07 +0530

    Fix font imports, add placeholder-data, fix query route and dashboard page

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,6 +1,9 @@
-// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
-// https://nextjs.org/learn/dashboard-app/fetching-data
-const users = [
+import { User, Customer, Invoice, Revenue } from './definitions';
+
+// This file contains placeholder data that you'll be replacing with real data from your database.
+// The data types are defined in the definitions.ts file.
+
+export const users: User[] = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
@@ -9,13 +12,7 @@ const users = [
   },
 ];
 
-const customers = [
-  {
-    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
-    email: 'evil@rabbit.com',
-    image_url: '/customers/evil-rabbit.png',
-  },
+export const customers: Customer[] = [
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
     name: 'Delba de Oliveira',
@@ -29,10 +26,40 @@ const customers = [
     image_url: '/customers/lee-robinson.png',
   },
   {
-    id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+    id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
+    name: 'Hector Simpson',
+    email: 'hector@simpson.com',
+    image_url: '/customers/hector-simpson.png',
+  },
+  {
+    id: '50ca3e18-62cd-11ee-8c99-0242ac120002',
+    name: 'Steven Tey',
+    email: 'steven@tey.com',
+    image_url: '/customers/steven-tey.png',
+  },
+  {
+    id: '3958dc9e-787f-4377-85e9-fec4b6a6442a',
+    name: 'Steph Dietz',
+    email: 'steph@dietz.com',
+    image_url: '/customers/steph-dietz.png',
+  },
+  {
+    id: '76d65c26-f784-44a2-ac19-586678f3c2fe',
     name: 'Michael Novotny',
     email: 'michael@novotny.com',
     image_url: '/customers/michael-novotny.png',
+  },
+  {
+    id: 'd6e15727-9fe1-4961-8c5b-ea44b9bdc4b8',
+    name: 'Evil Rabbit',
+    email: 'evil@rabbit.com',
+    image_url: '/customers/evil-rabbit.png',
+  },
+  {
+    id: '126eed9c-c90c-4ef6-a4a8-fcf7408d3c66',
+    name: 'Emil Kowalski',
+    email: 'emil@kowalski.com',
+    image_url: '/customers/emil-kowalski.png',
   },
   {
     id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
@@ -41,14 +68,14 @@ const customers = [
     image_url: '/customers/amy-burns.png',
   },
   {
-    id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
+    id: '13D07535-C59E-4157-A011-F8D2EF4E2C48',
     name: 'Balazs Orban',
     email: 'balazs@orban.com',
     image_url: '/customers/balazs-orban.png',
   },
 ];
 
-const invoices = [
+export const invoices: Invoice[] = [
   {
     customer_id: customers[0].id,
     amount: 15795,
@@ -80,13 +107,13 @@ const invoices = [
     date: '2023-08-05',
   },
   {
-    customer_id: customers[2].id,
+    customer_id: customers[7].id,
     amount: 54246,
     status: 'pending',
     date: '2023-07-16',
   },
   {
-    customer_id: customers[0].id,
+    customer_id: customers[6].id,
     amount: 666,
     status: 'pending',
     date: '2023-06-27',
@@ -125,11 +152,35 @@ const invoices = [
     customer_id: customers[2].id,
     amount: 1000,
     status: 'paid',
-    date: '2022-06-05',
+    date: '2023-06-05',
+  },
+  {
+    customer_id: customers[0].id,
+    amount: 8945,
+    status: 'paid',
+    date: '2023-10-01',
+  },
+  {
+    customer_id: customers[2].id,
+    amount: 1000,
+    status: 'paid',
+    date: '2023-10-18',
+  },
+  {
+    customer_id: customers[0].id,
+    amount: 15795,
+    status: 'pending',
+    date: '2022-12-06',
+  },
+  {
+    customer_id: customers[1].id,
+    amount: 20348,
+    status: 'pending',
+    date: '2022-11-14',
   },
 ];
 
-const revenue = [
+export const revenue: Revenue[] = [
   { month: 'Jan', revenue: 2000 },
   { month: 'Feb', revenue: 1800 },
   { month: 'Mar', revenue: 2200 },
@@ -144,4 +195,3 @@ const revenue = [
   { month: 'Dec', revenue: 4800 },
 ];
 
-export { users, customers, invoices, revenue };
